a67c77132ca8c5013bb24abf58dfbc7d540acf4b
flyboy-byte <logan07night@gmail.com>
2026-04-29 19:35:04 -0600

    Password-Manager

---
+++ app/src/main/java/com/example/passwordmanager/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.passwordmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.passwordmanager.ui.theme.PasswordManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PasswordManagerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // Empty page
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PasswordManagerTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            // Empty page
+        }
+    }
+}
